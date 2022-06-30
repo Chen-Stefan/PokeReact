ebe2a88e36631f20ed783338f935784cc0c84fdd
Stefan Chen <zchen158@my.bcit.ca>
2022-06-29 22:08:01 -0700

    Display individual pokemon names using state and props

--- poke_practice/src/components/pokemon/PokemonCard.js
+++ poke_practice/src/components/pokemon/PokemonCard.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
 
 class PokemonCard extends Component {
+  state = {
+    name: '',
+    imageUrl: '',
+    pokemonIndex: ''
+  }
+
   render() {
+    const name = this.props.name
+    const url = this.props.url
+
     return (
       <div className='col-md-3 col-sm-6 mb-5'>
         <div className='card'>
           <div className='card-header'>
-            <h1>Stefan</h1>
+            <h1>{name}</h1>
           </div>
         </div>
       </div>
--- poke_practice/src/components/pokemon/PokemonList.js
+++ poke_practice/src/components/pokemon/PokemonList.js
@@ -1,34 +1,38 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import PokemonCard from './PokemonCard'
+import React, { Component } from "react";
+import axios from "axios";
+import PokemonCard from "./PokemonCard";
 
 class PokemonList extends Component {
   state = {
     url: "https://pokeapi.co/api/v2/pokemon/",
-    pokemon: null
-  }
+    pokemon: null,
+  };
 
   async componentDidMount() {
-    const res =  await axios.get(this.state.url)
+    const res = await axios.get(this.state.url);
     // res.data 是API传过来的整个Json数据
-    this.setState({pokemon: res.data['results']})
+    this.setState({ pokemon: res.data["results"] });
   }
 
   render() {
     return (
       <React.Fragment>
-      {this.state.pokemon ? (
-        <div className='row'>
-          {this.state.pokemon.map(pokemon => (
-            <PokemonCard />
-          ))}
-        </div>
-      ) : (
-        <h1>Loading Pokemons...</h1>
-      )}
+        {this.state.pokemon ? (
+          <div className="row">
+            {this.state.pokemon.map((pokemon) => (
+              <PokemonCard
+                key={pokemon.name}
+                name={pokemon.name}
+                url={pokemon.url}
+              />
+            ))}
+          </div>
+        ) : (
+          <h1>Loading Pokemons...</h1>
+        )}
       </React.Fragment>
-    )  
+    );
   }
 }
 
-export default PokemonList
+export default PokemonList;
