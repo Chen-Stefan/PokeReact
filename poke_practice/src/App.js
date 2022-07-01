817d34ec5a20193c799130b2e36596c41c28cbc6
Stefan Chen <zchen158@my.bcit.ca>
2022-06-30 20:32:39 -0700

    Install and create react routes

--- poke_practice/src/App.js
+++ poke_practice/src/App.js
@@ -1,17 +1,24 @@
-import logo from './logo.svg';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css';
-import Navbar from './components/layout/Navbar'
-import Dashboard from './components/layout/Dashboard';
+import logo from "./logo.svg";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Dashboard from "./components/layout/Dashboard";
+import Pokemon from "./components/pokemon/Pokemon";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar /> 
-      <div className="container">
-        <Dashboard />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route exact path="/" element= {<Dashboard />} />
+            <Route exact path="/pokemon/:pokemonIndex" element= {<Pokemon />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
