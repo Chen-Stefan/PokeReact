d155d6657beca1681a2724743a39782d45e988c4
Stefan Chen <zchen158@my.bcit.ca>
2022-07-01 23:44:52 -0700

    Use axios to grab pokemon name from pokemon url

--- poke_practice/src/components/pokemon/Pokemon.js
+++ poke_practice/src/components/pokemon/Pokemon.js
@@ -2,6 +2,19 @@ import React, { Component } from 'react'
 import axios from 'axios' 
 
 class Pokemon extends Component {
+  state = {
+    name: '',
+    pokemonIndex: '',
+    imageUrl: ''
+  }
+
+  async componentDidMount() {
+    const {pokemonIndex} = this.props.match.params
+
+    //URLs for pokemon information
+    const pokemonUrl = `https://pokeapi.co/api/v2/pokemon${pokemonIndex}`
+    const pokemonSpeciesUrl = `https://pokeapi.co/api/v2/pokemon-species/${pokemonIndex}`
+  }
   render() {
     return (
       <div>Pokemon</div>
