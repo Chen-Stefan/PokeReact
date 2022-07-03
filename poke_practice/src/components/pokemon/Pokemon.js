3740d667433e7ea459413d16ca828e7651bd3f86
Stefan Chen <zchen158@my.bcit.ca>
2022-07-02 23:58:49 -0700

    Grab every stat from pokemonUrl inside componentdidmount

--- poke_practice/src/components/pokemon/Pokemon.js
+++ poke_practice/src/components/pokemon/Pokemon.js
@@ -1,50 +1,107 @@
-import React, { Component } from 'react'
-import axios from 'axios' 
+import React, { Component } from "react";
+import axios from "axios";
 
 class Pokemon extends Component {
-
   state = {
-    name: '',
-    pokemonIndex: '',
-    imageUrl: '',
+    name: "",
+    pokemonIndex: "",
+    imageUrl: "",
     types: [],
-    description: '',
+    description: "",
     stats: {
-      hp: '',
-      attack: '',
-      defense: '',
-      speed: '',
-      specialAttack: '',
-      specialDefense: ''
+      hp: "",
+      attack: "",
+      defense: "",
+      speed: "",
+      specialAttack: "",
+      specialDefense: "",
     },
-    height: '',
-    weight: '',
-    eggGroup: '',
-    abilities: '',
-    genderRatioMale: '',
-    genderRatioFemale: '',
-    evs: '',
-    hatchSteps: ''
-  }
+    height: "",
+    weight: "",
+    eggGroup: "",
+    abilities: "",
+    genderRatioMale: "",
+    genderRatioFemale: "",
+    evs: "",
+    hatchSteps: "",
+  };
 
   async componentDidMount() {
-    const {pokemonIndex} = this.props.match.params
+    const { pokemonIndex } = this.props.match.params;
     //URLs for pokemon information
-    const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonIndex}`
-    const pokemonSpeciesUrl = `https://pokeapi.co/api/v2/pokemon-species/${pokemonIndex}`
+    const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonIndex}`;
+    const pokemonSpeciesUrl = `https://pokeapi.co/api/v2/pokemon-species/${pokemonIndex}`;
 
-    const pokemonRes = await axios.get(pokemonUrl)
-    const name = pokemonRes.data.name
-    const imageUrl = pokemonRes.data.sprites.front_default
+    const pokemonRes = await axios.get(pokemonUrl);
+    const name = pokemonRes.data.name;
+    const imageUrl = pokemonRes.data.sprites.front_default;
 
-    let {hp, attack, defense, speed, specialAttack, specialDefense} = ''
+    let { hp, attack, defense, speed, specialAttack, specialDefense } = "";
+
+    pokemonRes.data.stats.map((stat) => {
+      switch (stat.stat.name) {
+        case "hp":
+          hp = stat["base_stat"];
+          break;
+        case "attack":
+          attack = stat["base_stat"];
+          break;
+        case "defense":
+          defense = stat["base_stat"];
+          break;
+        case "speed":
+          speed = stat["base_stat"];
+          break;
+        case "special-attack":
+          specialAttack = stat["base_stat"];
+          break;
+        case "special-defense":
+          specialDefense = stat["base_stat"];
+          break;
+      }
+    });
+    // Convert height from pokemon measures to cm, convert weight to kg
+    const height =
+      Math.round(pokemonRes.data.height * 0.328084 * 0.0001 * 2.54 * 100) / 100;
+    const weight =
+      Math.round(pokemonRes.data.weight * 0.220462 * 0.0001 * 0.453592 * 100) /
+      100;
+
+    const types = pokemonRes.data.types.map((type) => type.type.name);
+    const abilities = pokemonRes.data.abilities.map((ability) => {
+      return ability.ability.name
+        .toLowerCase()
+        .split("-")
+        .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+        .join(" ");
+    });
+
+    const evs = pokemonRes.data.stats
+      .filter((stat) => {
+        if (stat.effort > 0) {
+          return true;
+        }
+        return false;
+      })
+      .map((stat) => {
+        return `${stat.effort} ${stat.stat.name}`
+          .toLowerCase()
+          .split("-")
+          .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+          .join(" ");
+      }).join(', ')
+
+      //Get pokemon description. Have to use the pokemonSpecies url
+      await axios.get(pokemonSpeciesUrl)
   }
 
   render() {
     return (
-      <div><h1>{this.state.name}</h1></div>
-    )
+      <div>
+        <h1>{this.state.name}</h1>
+      </div>
+    );
   }
 }
 
-export default Pokemon
+export default Pokemon;
